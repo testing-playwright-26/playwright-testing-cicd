f25ab5ee49d639e86a87c096c393afcdb93c7b6d
testing-playwright-26 <sandhya.x.boiri@gmailcom>
2026-09-07 19:12:49 +0530

    Run Playwright browser in headless mode for CI

--- src/test/java/PlaywrightScriptPractice/BaseTest.java
+++ src/test/java/PlaywrightScriptPractice/BaseTest.java
@@ -37,7 +37,7 @@ public class BaseTest {
     	System.out.println("----****************------- ");
     	System.out.println("---BeforeAll---");
     	playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         
        
     }
